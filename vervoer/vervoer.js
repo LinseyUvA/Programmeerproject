e2a06edfa6562c4b2885587b980744f7c5d26f19
LinseyUvA <linseyschaap@tele2.nl>
2018-06-06 11:13:09 +0200

    diagram toegevoegd in doc

--- vervoer/vervoer.js
+++ vervoer/vervoer.js
@@ -11,7 +11,8 @@ window.onload = function() {
 
   d3.queue()
     .defer(d3.json, "reizigerskilometers.json")
-    .defer(d3.xml, "Nederland.svg")
+    .defer(d3.json, "vervoerswijze.json")
+    .defer(d3.xml, "Netherlands.svg")
     .awaitAll(grafieken);
 
   function grafieken(error, response) {
@@ -24,26 +25,35 @@ window.onload = function() {
 
 function kaart(response) {
 
-  console.log(response)
   // creëer een infoKnop
   var infoKnop = d3.select("body").append("g")
                    .attr("class", "tooltipje");
 
   // data opslaan
   var reizigerskilometers = response["0"]["data"]
-  document.body.appendChild(response[1].documentElement);
+  var vervoerswijze = response["1"]["data"]
+  document.body.appendChild(response[2].documentElement);
 
-  var svg = d3.selectAll("#Noord-Holland, #Zuid-Holland, #Zeeland, #Flevoland, #Utrecht, #Noord-Brabant, #Friesland, #Groningen, #Drenthe, #Overijssel, #Gelderland, #Limburg")
+  dataReizigers = []
+  for (var i = 0; i < reizigerskilometers.length; i++) {
+    if (reizigerskilometers[i].Periode == "2010" && reizigerskilometers[i].Vervoerswijze == "Totaal") {
+      dataReizigers.push(reizigerskilometers[i])
+    }
+  }
+  // var kleur = d3.scaleLinear([0, 9])
+  //               .range(["#dadaeb", "#3f007d"])
+
+  var svg = d3.selectAll("#Zeeland, #Noord-Holland, #Zuid-Holland, #Flevoland, #Utrecht, #Noord-Brabant, #Friesland, #Groningen, #Drenthe, #Overijssel, #Gelderland, #Limburg")
               .style("fill", "Indigo")
-              .data(reizigerskilometers)
+              .data(dataReizigers)
               // maak de staven interactief
               .on("mouseover", function() {
-                  infoKnop.style("display", null);
-                  d3.select(this).style("fill", "SlateGray");})
+                infoKnop.style("display", null);
+                d3.select(this).style("fill", "SlateGray");})
               .on("mouseout", function() {
-                  infoKnop.style("display", null);
-                  d3.select(this).style("fill", "Indigo");})
+                infoKnop.style("display", null);
+                d3.select(this).style("fill", "Indigo");})
               .on("mousemove", function(d) {
-                  infoKnop.html("Vanuit " + d.Provincie + " overnachten duizend mensen in Nederland (2012)<br/>")})
+                infoKnop.html("Er zijn in " + d.Provincie + " zijn er " + d.Afstand + " reizigerskilometers <br/>")})
 
 }
