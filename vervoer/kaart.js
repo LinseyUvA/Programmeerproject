848c5f600d87668cde1f952eeeb47c90470521e8
LinseyUvA <linseyschaap@tele2.nl>
2018-06-15 11:29:15 +0200

    site aangepast voor werkcollege

--- vervoer/kaart.js
+++ vervoer/kaart.js
@@ -42,66 +42,66 @@ function slider() {
   var grafiekHoogte = hoogte - marge.boven - marge.beneden;
   var grafiekBreedte = breedte - marge.rechts - marge.links;
 
-  var svg = d3.select("#sliderContainer")
-    .append("svg")
-    .attr("width", grafiekBreedte + marge.links + marge.rechts)
-    .attr("height", grafiekHoogte + marge.boven + marge.beneden);
-
-  var formatDateIntoYear = d3.timeFormat("%Y")
-
-  var startDatum = new Date("2010");
-  var eindDatum = new Date("2014");
-
-  var moving = false;
-  var currentValue = 0;
-  var targetValue = grafiekBreedte;
-
-  var playButton = d3.select("#play-button");
-
-  var x = d3.scaleTime()
-      .domain([startDatum, eindDatum])
-      .range([0, targetValue])
-      .clamp(true);
-
-  var slider = svg.append("g")
-      .attr("class", "slider")
-      .attr("transform", "translate(" + marge.links + "," + grafiekHoogte/5 + ")");
-
-  slider.append("line")
-      .attr("class", "track")
-      .attr("x1", x.range()[0])
-      .attr("x2", x.range()[1])
-      .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-      .attr("class", "track-inset")
-      .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-      .attr("class", "track-overlay")
-      .call(d3.drag()
-          .on("start.interrupt", function() { slider.interrupt(); })
-          .on("start drag", function() {
-            currentValue = d3.event.x;
-            update(x.invert(currentValue));
-          })
-      );
-
-  slider.insert("g", ".track-overlay")
-      .attr("class", "ticks")
-      .attr("transform", "translate(0," + 18 + ")")
-      .selectAll("text")
-      .data(x.ticks(4))
-      .enter()
-      .append("text")
-      .attr("x", x)
-      .attr("y", 10)
-      .attr("text-anchor", "middle")
-      .text(function(d) { return formatDateIntoYear(d); });
-
-  var handle = slider.insert("circle", ".track-overlay")
-      .attr("class", "handle")
-      .attr("r", 9);
-
-  var label = slider.append("text")
-      .attr("class", "label")
-      .attr("text-anchor", "middle")
-      .text(formatDateIntoYear(startDatum))
-      .attr("transform", "translate(0," + (-25) + ")")
+  // var svg = d3.select("#sliderContainer")
+  //   .append("svg")
+  //   .attr("width", grafiekBreedte + marge.links + marge.rechts)
+  //   .attr("height", grafiekHoogte + marge.boven + marge.beneden);
+  //
+  // var formatDateIntoYear = d3.timeFormat("%Y")
+  //
+  // var startDatum = new Date("2010");
+  // var eindDatum = new Date("2014");
+  //
+  // var moving = false;
+  // var currentValue = 0;
+  // var targetValue = grafiekBreedte;
+  //
+  // var playButton = d3.select("#play-button");
+  //
+  // var x = d3.scaleTime()
+  //     .domain([startDatum, eindDatum])
+  //     .range([0, targetValue])
+  //     .clamp(true);
+  //
+  // var slider = svg.append("g")
+  //     .attr("class", "slider")
+  //     .attr("transform", "translate(" + marge.links + "," + grafiekHoogte/5 + ")");
+  //
+  // slider.append("line")
+  //     .attr("class", "track")
+  //     .attr("x1", x.range()[0])
+  //     .attr("x2", x.range()[1])
+  //     .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+  //     .attr("class", "track-inset")
+  //     .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+  //     .attr("class", "track-overlay")
+  //     .call(d3.drag()
+  //         .on("start.interrupt", function() { slider.interrupt(); })
+  //         .on("start drag", function() {
+  //           currentValue = d3.event.x;
+  //           update(x.invert(currentValue));
+  //         })
+  //     );
+  //
+  // slider.insert("g", ".track-overlay")
+  //     .attr("class", "ticks")
+  //     .attr("transform", "translate(0," + 18 + ")")
+  //     .selectAll("text")
+  //     .data(x.ticks(4))
+  //     .enter()
+  //     .append("text")
+  //     .attr("x", x)
+  //     .attr("y", 10)
+  //     .attr("text-anchor", "middle")
+  //     .text(function(d) { return formatDateIntoYear(d); });
+  //
+  // var handle = slider.insert("circle", ".track-overlay")
+  //     .attr("class", "handle")
+  //     .attr("r", 9);
+  //
+  // var label = slider.append("text")
+  //     .attr("class", "label")
+  //     .attr("text-anchor", "middle")
+  //     .text(formatDateIntoYear(startDatum))
+  //     .attr("transform", "translate(0," + (-25) + ")")
 }
